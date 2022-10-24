3538fc74d20ae2b4343a2400167c8166bd73cfae
Pavel-Zolenko <pobedu8@gmail.com>
2022-10-24 21:19:18 +0300

    hw6_5

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,8 +1,12 @@
 const inputEl = document.querySelector("#name-input");
 const spanEl = document.querySelector("#name-output");
+ console.log(inputEl)
 
 const onAssignTextInput = (event) => {
     spanEl.textContent = event.currentTarget.value;
+    if (event.currentTarget.value === "") {
+        spanEl.textContent = "Anonymous!"
+    }
     }
 
 inputEl.addEventListener("input", onAssignTextInput);
